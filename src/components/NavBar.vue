ad18513f4475a80db6a8bf13648804d5efa7f405
vitali.carolina <caritovitali@gmail.com>
2022-06-15 23:33:08 -0300

    cambios reentrega

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -20,7 +20,7 @@
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
               </svg>
             <span class="flex absolute -mt-5 ml-4">
-              <span class="absolute top-0 right-0 inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-red-100 transform translate-x-1/2 -translate-y-1/2 bg-red-600 rounded-full"></span>
+              <span class="absolute top-0 right-0 inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-red-100 transform translate-x-1/2 -translate-y-1/2 bg-red-600 rounded-full">{{ cartQty }}</span>
               </span>
           </a>
           <!-- Sign In / Register      -->
@@ -51,7 +51,7 @@
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
               </svg>
             <span class="flex absolute -mt-5 ml-4">
-              <span class="absolute top-0 right-0 inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-red-100 transform translate-x-1/2 -translate-y-1/2 bg-red-600 rounded-full"></span>
+              <span class="absolute top-0 right-0 inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-red-100 transform translate-x-1/2 -translate-y-1/2 bg-red-600 rounded-full">{{ cartQty }}</span>
               </span>
       </a>
       <a class="navbar-burger self-center mr-12 xl:hidden" href="#">
@@ -74,7 +74,7 @@
     name: 'nav-bar',
     props: {
    
-  },
+     },
    components: {
   },
     data () {
@@ -82,20 +82,20 @@
       }
     },
     methods: {
-        verCarrito(){
+/*         verCarrito(){
           this.$emit('ver-carrito', true)
-       },
+       }, */
           logOut(){
           this.$emit('log-out', true)
        },
 
       ...mapActions('user', ['setUser']),
-    ...mapActions('user', ['setCart']),
+      ...mapActions('cart', ['setCart','verCarrito']),
 
     },
  computed: {
         ...mapGetters('user', ['user']),
-  
+        ...mapGetters('cart', ['cart', 'cartQty']),
         
   }
 }
